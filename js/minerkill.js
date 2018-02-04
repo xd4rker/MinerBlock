26fef58e686677eee25c919f614c55b7ca0175cc
xd4rker <xd4rker@gmail.com>
2018-02-04 14:06:30 +0000

    Updated filters list

--- js/minerkill.js
+++ js/minerkill.js
@@ -9,23 +9,25 @@ function triggerMblockEvent(miner) {
 	document.dispatchEvent(event);
 }
 
-for(let name in this) {
+setTimeout(function() {
+	for(let name in this) {
 
-	if(name === 'webkitStorageInfo') {
-		continue;
+		if(name === 'webkitStorageInfo') {
+			continue;
+		}
+
+		// Check Coinhive like miners
+
+		if(	this[name]
+			&& typeof this[name] !== 'undefined'
+			&& typeof this[name].isRunning === 'function'
+			&& typeof this[name].stop === 'function'
+			&& typeof this[name]._siteKey === 'string'
+			) {
+			console.log('[+] Coinhive miner found, stopping...');
+			this[name].stop();
+			this[name] = null;
+			triggerMblockEvent('coinhive.com (inline)');
+		}
 	}
-
-	// Check coinhive miner
-
-	if(	this[name]
-		&& typeof this[name] !== 'undefined'
-		&& typeof this[name].isRunning === 'function'
-		&& typeof this[name].stop === 'function'
-		&& typeof this[name]._siteKey === 'string'
-		) {
-		console.log('[+] Coinhive miner found, stopping...');
-		this[name].stop();
-		this[name] = null;
-		triggerMblockEvent('coinhive.com (inline)');
-	}
-}
+}, 2000);
