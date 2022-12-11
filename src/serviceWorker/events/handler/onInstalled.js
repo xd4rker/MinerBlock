036ba3084f10dd0eb402cf5941b296dd7e92312a
mandano <mandanothedano@posteo.de>
2022-12-11 18:58:48 +0100

    docs: added todo

--- src/serviceWorker/events/handler/onInstalled.js
+++ src/serviceWorker/events/handler/onInstalled.js
@@ -6,6 +6,7 @@
  * @returns {Promise<void>}
  */
 export async function setup(initSettings, initBrowser) {
+    //TODO: run settings migration from 1.x.x versions
     await initSettings.run();
 
     await initBrowser.run();
