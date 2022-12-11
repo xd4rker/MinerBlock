99cb89d153765750fb517a39ac316e4d85901708
mandano <mandanothedano@posteo.de>
2022-12-11 18:24:30 +0100

    feat: add test case

---
+++ test/integration/serviceWorker/interactors/getWhitelistStatus.test.js
@@ -0,0 +1,30 @@
+import { strict as assert } from 'assert';
+import {FakeLocalStorage} from "../../../serviceWorker/repositories/adapters/FakeLocalStorage.js";
+import {Logger} from "../../../../src/serviceWorker/adapters/Logger.js";
+import {SettingsRepository} from "../../../../src/serviceWorker/repositories/SettingsRepository.js";
+import {GetWhitelistStatus} from "../../../../src/serviceWorker/interactors/GetWhitelistStatus.js";
+import {GetWhitelist} from "../../../../src/serviceWorker/interactors/GetWhitelist.js";
+
+
+it('checks if domain whitelisted', async () => {
+    const logger = new Logger();
+
+    const domain = 'lala';
+    const storage = new FakeLocalStorage({
+        'mbSettings': {
+            "whiteList": [domain]
+        }
+    });
+
+    const settingsRepository = new SettingsRepository(storage, logger);
+
+    const getWhitelistStatus = new GetWhitelistStatus(
+        domain,
+        new GetWhitelist(settingsRepository, logger),
+        logger
+    );
+
+    const isWhitelisted = await getWhitelistStatus.run();
+
+    assert.equal(isWhitelisted, true);
+});
