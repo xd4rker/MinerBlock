2aa2ae3ad046de8b80ef88e4fd9772afadeb739e
mandano <mandanothedano@posteo.de>
2022-12-26 13:00:19 +0100

    refactor: do not use config.js for options and popUp page
    intantiate directly in main.js

--- src/common/config.js
+++
@@ -1,3 +0,0 @@
-const logger = new Logger(Logger.LEVEL_DEBUG);
-
-const _browser = new Browser(new Chrome());
--- src/options/main.js
+++ src/options/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const logger = new Logger(Logger.LEVEL_DEBUG);
+const _browser = new Browser(new Chrome());
+
 document.addEventListener('DOMContentLoaded', function () {
     const options = new Options(_browser);
 
