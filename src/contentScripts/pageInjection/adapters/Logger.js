7d6edd2ab2955b860271669991c9c9f17160c3eb
mandano <mandanothedano@posteo.de>
2023-02-12 16:01:19 +0100

    fix: duplicate logger class so that it can be injected

---
+++ src/contentScripts/pageInjection/adapters/Logger.js
@@ -0,0 +1,34 @@
+class Logger {
+    /** @type {number} */
+    static LEVEL_ERROR = -1;
+    /** @type {number} */
+    static LEVEL_DEBUG = 3;
+
+    #logLevel = Logger.LEVEL_ERROR;
+
+    constructor(logLevel) {
+        this.#logLevel = logLevel;
+    }
+
+    /**
+     * @param {string} message
+     * @param {string?} where
+     * @param {any?} details
+     */
+    debug (message, where = undefined, details = undefined) {
+        if (Logger.LEVEL_DEBUG <= this.#logLevel) {
+            console.log(where, message, details);
+        }
+    }
+
+    /**
+     * @param {string} message
+     * @param {string?} where
+     * @param {any?} details
+     */
+    error (message, where = undefined, details = undefined) {
+        if (Logger.LEVEL_ERROR <= this.#logLevel) {
+            console.log(where, message, details);
+        }
+    }
+}
