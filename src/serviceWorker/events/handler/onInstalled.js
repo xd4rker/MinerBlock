ad41832c4e7f44670ac4df758181f556a3fbccb4
mandano <mandanothedano@posteo.de>
2023-02-24 18:17:32 +0100

    feat: register minerBlock

--- src/serviceWorker/events/handler/onInstalled.js
+++ src/serviceWorker/events/handler/onInstalled.js
@@ -3,11 +3,14 @@
  *
  * @param {InitSettings} initSettings
  * @param {InitBrowser} initBrowser
+ * @param {RegisterMinerBlocker} registerMinerBlocker
  * @returns {Promise<void>}
  */
-export async function setup(initSettings, initBrowser) {
+export async function setup(initSettings, initBrowser, registerMinerBlocker) {
     //TODO: run settings migration from 1.x.x versions
     await initSettings.run();
 
     await initBrowser.run();
+
+    await registerMinerBlocker.run();
 }
